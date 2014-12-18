f0f1e495854df9a31f57ee7bfb03a9d13db0d6e0
rveeraghattam <rajeev.veeraghattam@gmail.com>
2014-12-18 12:24:38 -0500

    GameSpec

--- Tests/spec/GameSpec.js
+++ Tests/spec/GameSpec.js
@@ -2,7 +2,7 @@ describe("Testing Jasmine", function() {
   it("Will test to make sure true = true", function() {
     expect(true).toBe(true);
   });
-  //it("Will test to make sure true = false", function() {
-  //  expect(true).toBe(false);
-  //});
+  it("Will test to make sure true = false", function() {
+    expect(true).toBe(false);
+  });
 });
