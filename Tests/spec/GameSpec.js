c92b5b5f0a5c762c65aa73c93ddcfcdadd5da951
Nate Thornton <nthornton@marketstar.com>
2014-12-18 10:16:33 -0700

    Added jasmine to the project so we can use it for tests.

---
+++ Tests/spec/GameSpec.js
@@ -0,0 +1,8 @@
+describe("Testing Jasmine", function() {
+  it("Will test to make sure true = true", function() {
+    expect(true).toBe(true);
+  });
+  //it("Will test to make sure true = false", function() {
+  //  expect(true).toBe(false);
+  //});
+});
