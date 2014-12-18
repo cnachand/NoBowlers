d1dff6a08801744c1e99c4ec9bf11df1474c6c28
Nate Thornton <nthornton@marketstar.com>
2014-12-18 14:34:16 -0700

    did score calculations with tests.

--- Tests/spec/GameSpec.js
+++ Tests/spec/GameSpec.js
@@ -34,3 +34,71 @@ describe("WeBowl", function(){
 		expect(game.score()).toBe(20);
 	});
 });
+
+describe("WeBowl2", function() {
+	var game;
+
+	beforeEach(function(){
+		game = new WeBowl();
+	});
+
+	it("Can throw gutter game", function(){
+		game.roll2(game.newFrame(), 0, 0);
+		game.roll2(game.newFrame(), 0, 0);
+		game.roll2(game.newFrame(), 0, 0);
+		game.roll2(game.newFrame(), 0, 0);
+		game.roll2(game.newFrame(), 0, 0);
+		game.roll2(game.newFrame(), 0, 0);
+		game.roll2(game.newFrame(), 0, 0);
+		game.roll2(game.newFrame(), 0, 0);
+		game.roll2(game.newFrame(), 0, 0);
+		game.roll2(game.newFrame(), 0, 0);
+
+		expect(game.score2()).toBe(0);
+	});
+
+	it("Can throw perfect game", function() {
+		game.roll2(game.newFrame(), 10, 0);
+		game.roll2(game.newFrame(), 10, 0);
+		game.roll2(game.newFrame(), 10, 0);
+		game.roll2(game.newFrame(), 10, 0);
+		game.roll2(game.newFrame(), 10, 0);
+		game.roll2(game.newFrame(), 10, 0);
+		game.roll2(game.newFrame(), 10, 0);
+		game.roll2(game.newFrame(), 10, 0);
+		game.roll2(game.newFrame(), 10, 0);
+		game.roll2(game.newFrame(), 10, 10, 10);
+
+		expect(game.score2()).toBe(300);
+	});
+
+	it("Can roll a score of 20", function() {
+		game.roll2(game.newFrame(), 1, 1);
+		game.roll2(game.newFrame(), 1, 1);
+		game.roll2(game.newFrame(), 1, 1);
+		game.roll2(game.newFrame(), 1, 1);
+		game.roll2(game.newFrame(), 1, 1);
+		game.roll2(game.newFrame(), 1, 1);
+		game.roll2(game.newFrame(), 1, 1);
+		game.roll2(game.newFrame(), 1, 1);
+		game.roll2(game.newFrame(), 1, 1);
+		game.roll2(game.newFrame(), 1, 1);
+
+		expect(game.score2()).toBe(20);
+	});
+
+	it("Can roll a score of 15", function() {
+		game.roll2(game.newFrame(), 0, 1);
+		game.roll2(game.newFrame(), 1, 1);
+		game.roll2(game.newFrame(), 1, 0);
+		game.roll2(game.newFrame(), 1, 1);
+		game.roll2(game.newFrame(), 1, 1);
+		game.roll2(game.newFrame(), 0, 1);
+		game.roll2(game.newFrame(), 1, 1);
+		game.roll2(game.newFrame(), 0, 1);
+		game.roll2(game.newFrame(), 1, 0);
+		game.roll2(game.newFrame(), 1, 1);
+
+		expect(game.score2()).toBe(15);
+	});
+});
